efc51e80bc224c0feb6ffc4261243b19d361f8fd
daedlock <hossam.saraya@gmail.com>
2024-07-08 02:53:05 +0300

    cli to switch alacritty themes in sequence
    att n #next theme
    att p #prev theme
    att c #cur theme

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+#[derive(Debug, serde::Deserialize)]
+struct Theme {
+    import: Vec<String>,
+}
+
+fn get_current_theme() -> String {
+    // read ~/.config/alacritty/alacritty.toml
+    // and get the current theme from import key
+    let home_dir = std::env::var("HOME").unwrap();
+    let alac_path = format!("{}/.config/alacritty/alacritty.toml", home_dir);
+    let alacritty_conf_str = std::fs::read_to_string(alac_path).unwrap();
+    let t: Theme = toml::from_str(alacritty_conf_str.as_str()).unwrap();
+    if t.import.len() == 0 {
+        panic!("No theme found");
+    }
+    let theme_file = t.import.last().unwrap();
+    let theme_name = theme_file.split("/").last().unwrap().replace(".toml", "");
+    theme_name
+}
+
+fn get_theme_list() -> Vec<String> {
+    let output = std::process::Command::new("alacritty-theme-switcher")
+        .arg("-l")
+        .output()
+        .expect("failed to execute process");
+
+    let themes = String::from_utf8_lossy(&output.stdout)
+        .split("\n")
+        .map(|x| x.trim().to_string())
+        .collect::<Vec<String>>()
+        .clone();
+    themes
+}
+
+fn get_cur_idx() -> usize {
+    let themes = get_theme_list();
+    let cur_theme = get_current_theme();
+    themes
+        .iter()
+        .position(|x| x.as_str() == cur_theme.as_str())
+        .unwrap()
+}
+
+fn green(s: &str) -> String {
+    format!("\x1b[32m{}\x1b[0m", s)
+}
+fn bold(s: &str) -> String {
+    format!("\x1b[1m{}\x1b[0m", s)
+}
+fn switch_theme(theme: &str) {
+    let output = std::process::Command::new("alacritty-theme-switcher")
+        .arg(theme)
+        .output()
+        .expect("failed to execute process");
+    println!(
+        "↪️ Changed alacritty theme to: {}",
+        bold(green(theme).as_str())
+    );
+}
+
+fn main() {
+    let cur_theme = get_current_theme();
+    let themes = get_theme_list();
+    // execute shell command
+    let cur_idx = get_cur_idx();
+
+    // get arg (n or p)
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        let arg = args.get(1).unwrap();
+        if arg == "n" {
+            let next_idx = (cur_idx + 1) % themes.len();
+            let next_theme = themes.get(next_idx).unwrap();
+
+            switch_theme(next_theme);
+            return;
+        }
+        if arg == "p" {
+            let prev_idx = (cur_idx + themes.len() - 1) % themes.len();
+            let prev_theme = themes.get(prev_idx).unwrap();
+            switch_theme(prev_theme);
+            return;
+        }
+
+        if arg == "c" {
+            println!("{}", cur_theme);
+            return;
+        }
+    }
+}
